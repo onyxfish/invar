e44d029f7f8734daeb5c390a2d314f85b6003a73
cgroskopf <cgroskopf@tribune.com>
2011-06-16 09:23:55 -0500

    Added ivs3. Updated version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "invar",
-    version = "0.0.5",
+    version = "0.0.6",
     description = "Scripts for generating tiles from Mapnik configuration.",
     author='Christopher Groskopf',
     author_email='staringmonkey@gmail.com',
@@ -15,10 +15,13 @@ setup(
     ],
     scripts = [
         'ivtile',
-        'ivframe'
+        'ivframe',
+        'ivs3'
     ],
     install_requires = [
-        'argparse'
+        'argparse',
+        'eventlet',
+        'boto'
     ]
 )
 
