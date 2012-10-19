b133241ddebbd792d75f68dd36e23617ed0d17e6
Christopher Groskopf <staringmonkey@gmail.com>
2012-10-19 13:42:29 -0700

    Tweak description for future release.

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 invar
 =====
 
-Command line tools for generating map images from `Mapnik <http://mapnik.org/>`_ configuration.
+Command line tools for generating map images from `Mapnik <http://mapnik.org/>`_ configuration and uploading them to S3.
 
 invar is named after a `metal alloy <http://en.wikipedia.org/wiki/Invar>`_ frequently used in leveling rods and survey tapes.
 
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 
 setup(
     name = "invar",
-    version = "0.0.15",
-    description = "Scripts for generating tiles from Mapnik configuration.",
+    version = "0.0.16",
+    description = "Scripts for generating tiles from Mapnik configuration and storing them on S3.",
     author='Christopher Groskopf',
     author_email='staringmonkey@gmail.com',
     url='http://blog.apps.chicagotribune.com/',
