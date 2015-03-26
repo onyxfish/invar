a3fbbf3f43313582bbfc7ffa76dacee76560135c
Christopher Groskopf <staringmonkey@gmail.com>
2015-03-26 11:21:20 -0500

    Fix JoinableQueue bug. Closes #6.

--- invar/renderer.py
+++ invar/renderer.py
@@ -14,11 +14,11 @@ class Renderer(multiprocessing.Process):
     """
     A Mapnik renderer process.
     """
-    def __init__(self, tile_queues, config, width=constants.DEFAULT_WIDTH, height=constants.DEFAULT_HEIGHT, filetype=constants.DEFAULT_FILE_TYPE, buffer_size=None, skip_existing=False):
+    def __init__(self, tile_queue, config, width=constants.DEFAULT_WIDTH, height=constants.DEFAULT_HEIGHT, filetype=constants.DEFAULT_FILE_TYPE, buffer_size=None, skip_existing=False):
         multiprocessing.Process.__init__(self)
 
         self.config = config
-        self.tile_queues = tile_queues
+        self.tile_queue = tile_queue
         self.width = width
         self.height = height
         self.buffer_size = buffer_size if buffer_size else max(width, height)
@@ -30,18 +30,15 @@ class Renderer(multiprocessing.Process):
         mapnik.load_map(self.mapnik_map, self.config, True)
 
         self.map_projection = mapnik.Projection(self.mapnik_map.srs)
-        self.tile_projection = projections.GoogleProjection()  
+        self.tile_projection = projections.GoogleProjection()
 
         while True:
             tile_parameters = None
 
-            # Try to fetch a tile from any queue
-            for tile_queue in self.tile_queues:
-                try:
-                    tile_parameters = tile_queue.get_nowait()
-                    break 
-                except Queue.Empty:
-                    pass
+            try:
+                tile_parameters = self.tile_queue.get_nowait()
+            except Queue.Empty:
+                pass
 
             # Couldn't get tile parameters from any queue--all done
             if not tile_parameters:
@@ -53,12 +50,12 @@ class Renderer(multiprocessing.Process):
 
                 if os.path.exists(filename):
                     print 'Skipping %s' % (filename)
-                    tile_queue.task_done()
+                    self.tile_queue.task_done()
 
                     continue
 
             self.render(*tile_parameters)
-            tile_queue.task_done()
+            self.tile_queue.task_done()
 
     def render(self):
         """
@@ -68,10 +65,10 @@ class Renderer(multiprocessing.Process):
 
 class TileRenderer(Renderer):
     """
-    Renderer for tiles. 
+    Renderer for tiles.
     """
-    def __init__(self, tile_queues, config, width=constants.DEFAULT_WIDTH, height=constants.DEFAULT_HEIGHT, filetype=constants.DEFAULT_FILE_TYPE, buffer_size=None, skip_existing=False, **kwargs):
-        super(TileRenderer, self).__init__(tile_queues, config, width, height, filetype, buffer_size, skip_existing)
+    def __init__(self, tile_queue, config, width=constants.DEFAULT_WIDTH, height=constants.DEFAULT_HEIGHT, filetype=constants.DEFAULT_FILE_TYPE, buffer_size=None, skip_existing=False, **kwargs):
+        super(TileRenderer, self).__init__(tile_queue, config, width, height, filetype, buffer_size, skip_existing)
         self.grid = kwargs.get('grid', False)
         self.key =  kwargs.get('key', None)
         self.fields =  kwargs.get('fields', None)
@@ -91,71 +88,7 @@ class TileRenderer(Renderer):
         # Convert tile coords to LatLng
         ll0 = self.tile_projection.fromPixelToLL(px0, zoom);
         ll1 = self.tile_projection.fromPixelToLL(px1, zoom);
-        
-        # Convert LatLng to map coords
-        c0 = self.map_projection.forward(mapnik.Coord(ll0[0], ll0[1]))
-        c1 = self.map_projection.forward(mapnik.Coord(ll1[0], ll1[1]))
 
-        # Create bounding box for the render
-        bbox = mapnik.Box2d(c0.x, c0.y, c1.x, c1.y)
-
-        self.mapnik_map.zoom_to_box(bbox)
-        self.mapnik_map.buffer_size = self.buffer_size 
-
-        # Render image with default renderer
-        image = mapnik.Image(self.width, self.height)
-        mapnik.render(self.mapnik_map, image)
-        image.save(filename, self.filetype)
-        
-        if self.grid:
-            if self.key:
-                grid = mapnik.Grid(self.width, self.height)
-            else:
-                grid = mapnik.Grid(self.width, self.height, key=self.key)
-
-            fields = []
-
-            if self.fields:
-                fields.extend(self.fields)
-
-            mapnik.render_layer(self.mapnik_map,grid,layer=0,fields=fields)
-            # then encode the grid array as utf, resample to 1/4 the size, and dump features
-            # this comes from https://github.com/springmeyer/gridsforkids/blob/master/generate_tiles.py 
-            # with little consideration
-            grid_utf = grid.encode('utf', resolution=4, features=True)
-
-            # client code uses jsonp, so fake by wrapping in grid() callback
-            base, ext = os.path.splitext(filename)
-            grid_filename = '%s.grid.json' % base
-            print 'Rendering %s' % (grid_filename)
-
-            with open(grid_filename,'wb') as f:
-                f.write('grid(' + json.dumps(grid_utf) + ')')
-            
-            
-
-class FrameRenderer(Renderer):
-    """
-    Renderer for frames (centered map fragments).
-    """
-    def render(self, filename, latitude, longitude, zoom):
-        """
-        Render a single tile to a given filename.
-        """
-        print 'Rendering %s' % (filename)
-    
-        x, y = self.tile_projection.fromLLtoPixel([longitude, latitude], zoom) 
-
-        # Calculate pixel positions of bottom-left & top-right
-        half_width = self.width / 2
-        half_height = self.height / 2
-        px0 = (x - half_width, y + half_height)
-        px1 = (x + half_width, y - half_height)
-
-        # Convert tile coords to LatLng
-        ll0 = self.tile_projection.fromPixelToLL(px0, zoom);
-        ll1 = self.tile_projection.fromPixelToLL(px1, zoom);
-        
         # Convert LatLng to map coords
         c0 = self.map_projection.forward(mapnik.Coord(ll0[0], ll0[1]))
         c1 = self.map_projection.forward(mapnik.Coord(ll1[0], ll1[1]))
@@ -171,3 +104,66 @@ class FrameRenderer(Renderer):
         mapnik.render(self.mapnik_map, image)
         image.save(filename, self.filetype)
 
+        if self.grid:
+            if self.key:
+                grid = mapnik.Grid(self.width, self.height)
+            else:
+                grid = mapnik.Grid(self.width, self.height, key=self.key)
+
+            fields = []
+
+            if self.fields:
+                fields.extend(self.fields)
+
+            mapnik.render_layer(self.mapnik_map,grid,layer=0,fields=fields)
+            # then encode the grid array as utf, resample to 1/4 the size, and dump features
+            # this comes from https://github.com/springmeyer/gridsforkids/blob/master/generate_tiles.py
+            # with little consideration
+            grid_utf = grid.encode('utf', resolution=4, features=True)
+
+            # client code uses jsonp, so fake by wrapping in grid() callback
+            base, ext = os.path.splitext(filename)
+            grid_filename = '%s.grid.json' % base
+            print 'Rendering %s' % (grid_filename)
+
+            with open(grid_filename,'wb') as f:
+                f.write('grid(' + json.dumps(grid_utf) + ')')
+
+
+
+class FrameRenderer(Renderer):
+    """
+    Renderer for frames (centered map fragments).
+    """
+    def render(self, filename, latitude, longitude, zoom):
+        """
+        Render a single tile to a given filename.
+        """
+        print 'Rendering %s' % (filename)
+
+        x, y = self.tile_projection.fromLLtoPixel([longitude, latitude], zoom)
+
+        # Calculate pixel positions of bottom-left & top-right
+        half_width = self.width / 2
+        half_height = self.height / 2
+        px0 = (x - half_width, y + half_height)
+        px1 = (x + half_width, y - half_height)
+
+        # Convert tile coords to LatLng
+        ll0 = self.tile_projection.fromPixelToLL(px0, zoom);
+        ll1 = self.tile_projection.fromPixelToLL(px1, zoom);
+
+        # Convert LatLng to map coords
+        c0 = self.map_projection.forward(mapnik.Coord(ll0[0], ll0[1]))
+        c1 = self.map_projection.forward(mapnik.Coord(ll1[0], ll1[1]))
+
+        # Create bounding box for the render
+        bbox = mapnik.Box2d(c0.x, c0.y, c1.x, c1.y)
+
+        self.mapnik_map.zoom_to_box(bbox)
+        self.mapnik_map.buffer_size = self.buffer_size
+
+        # Render image with default renderer
+        image = mapnik.Image(self.width, self.height)
+        mapnik.render(self.mapnik_map, image)
+        image.save(filename, self.filetype)
